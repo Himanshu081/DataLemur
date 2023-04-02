008776217a2e86d67951d5a4ea13da577da3ddff
Himanshu Yadav <49179641+Himanshu081@users.noreply.github.com>
2023-04-02 11:58:49 +0530

    Add files via upload
    Q. Output the year (in ascending order) partitioned by product id, current year's spend, previous year's spend and year-on-year growth rate (percentage rounded to 2 decimal places).

---
+++ Y-on-Y Growth Rate [Wayfair SQL Question].sql
@@ -0,0 +1,11 @@
+--Y-on-Y Growth Rate [Wayfair SQL Question]
+
+with cte as(
+SELECT transaction_id,product_id,extract(year from transaction_date) as year,
+spend as curr_year_spend,
+lag(spend,1)over(PARTITION BY product_id order by transaction_date) as prev_year_spend
+FROM user_transactions)
+
+select year,product_id,curr_year_spend,prev_year_spend,
+round(((curr_year_spend - prev_year_spend)/prev_year_spend)*100,2)as yoy_rate
+from cte
