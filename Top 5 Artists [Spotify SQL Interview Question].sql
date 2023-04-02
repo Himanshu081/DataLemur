b9b49b77fc9cee4be11a5e3ba147c620d26730c8
Himanshu Yadav <49179641+Himanshu081@users.noreply.github.com>
2023-04-02 12:01:47 +0530

    Add files via upload

---
+++ Top 5 Artists [Spotify SQL Interview Question].sql
@@ -0,0 +1,16 @@
+--Top 5 Artists [Spotify SQL Interview Question]
+
+with cte as(
+SELECT artist_name,count(rank)as rnk
+FROM
+artists a 
+inner join 
+songs s on a.artist_id = s.artist_id
+inner join 
+global_song_rank gs on gs.song_id=s.song_id
+where gs.rank<=10
+group by artist_name),cte2 as(
+select artist_name,dense_rank()over(order by rnk desc)as artist_rank
+from cte)
+select * from cte2 where artist_rank<=5
+order by artist_rank asc,artist_name;
