051a50dcedb0a733745f487760d6ae26e13508da
Himanshu Yadav <49179641+Himanshu081@users.noreply.github.com>
2023-04-10 09:31:23 +0530

    Add files via upload

---
+++ 3-Topping Pizzas [McKinsey SQL Interview Question].sql
@@ -0,0 +1,8 @@
+--3-Topping Pizzas [McKinsey SQL Interview Question]
+SELECT concat(p1.topping_name,',',p2.topping_name,',',p3.topping_name)
+as pizza,p1.ingredient_cost+p2.ingredient_cost+p3.ingredient_cost as total_cost
+FROM 
+pizza_toppings p1,pizza_toppings p2,pizza_toppings p3
+where p1.topping_name<p2.topping_name
+and p2.topping_name<p3.topping_name
+order by total_cost desc,pizza;
