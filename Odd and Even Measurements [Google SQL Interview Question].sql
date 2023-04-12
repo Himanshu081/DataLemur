4b76f0a3ef350883676a180059c25e32fb24cfe3
Himanshu Yadav <49179641+Himanshu081@users.noreply.github.com>
2023-04-12 09:07:14 +0530

    Add files via upload

---
+++ Odd and Even Measurements [Google SQL Interview Question].sql
@@ -0,0 +1,12 @@
+--Odd and Even Measurements [Google SQL Interview Question]
+with cte AS(SELECT  *,
+row_number() over(PARTITION BY date(measurement_time) order by measurement_id)as row_id
+FROM measurements
+)
+select
+CAST(measurement_time AS date) AS measurement_day,
+sum(case when row_id%2!=0 then measurement_value else 0 end)odd_sum,
+sum(case when row_id%2=0 then measurement_value else 0 end)even_sum
+FROM cte
+group by CAST(measurement_time AS date)
+order by measurement_day ;
