dc80be2cdcfa1c8ca8aa2558833e22fa59edb5aa
Himanshu Yadav <49179641+Himanshu081@users.noreply.github.com>
2023-04-19 21:29:51 +0530

    Add files via upload

---
+++ Teams Power Users [Microsoft SQL Interview Question].sql
@@ -0,0 +1,8 @@
+--Teams Power Users [Microsoft SQL Interview Question]
+
+SELECT sender_id,count( DISTINCT message_id) as	message_count
+FROM messages
+where extract( month from sent_date)=8 and extract(year from sent_date)=2022
+group by sender_id 
+order by count(message_id) desc
+LIMIT 2;
