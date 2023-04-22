2380b4565eafc2c5c866468e4f071d57250f1b28
Himanshu Yadav <49179641+Himanshu081@users.noreply.github.com>
2023-04-23 04:58:52 +0530

    Add files via upload

---
+++ Duplicate Job Listings [Linkedin SQL Interview Question].sql
@@ -0,0 +1,11 @@
+
+--Duplicate Job Listings [Linkedin SQL Interview Question]
+with cte as(
+SELECT company_id,lower(title),lower(description),
+count(distinct job_id)as duplicate
+FROM job_listings
+group by company_id,lower(title),lower(description)
+having  count(distinct job_id)>1)
+
+select count(company_id) as co_w_duplicate_jobs
+from cte
