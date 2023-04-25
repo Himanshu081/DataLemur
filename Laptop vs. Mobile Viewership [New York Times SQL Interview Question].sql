e18ff1718219544e972cdfb57ffba3e3af8ffde6
Himanshu Yadav <49179641+Himanshu081@users.noreply.github.com>
2023-04-25 19:30:33 +0530

    Add files via upload

---
+++ Laptop vs. Mobile Viewership [New York Times SQL Interview Question].sql
@@ -0,0 +1,6 @@
+--Laptop vs. Mobile Viewership [New York Times SQL Interview Question]
+
+SELECT 
+sum(case when device_type='laptop' then 1 end) as laptop_views,
+sum(case when device_type='tablet' or device_type='phone' then 1 end) as mobile_views 
+FROM viewership;
