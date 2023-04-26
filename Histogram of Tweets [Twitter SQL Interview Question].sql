900c1c0e102b1cf3335e7ad817a18ce797b3d012
Himanshu Yadav <49179641+Himanshu081@users.noreply.github.com>
2023-04-26 08:05:22 +0530

    Add files via upload

---
+++ Histogram of Tweets [Twitter SQL Interview Question].sql
@@ -0,0 +1,8 @@
+--Histogram of Tweets [Twitter SQL Interview Question]
+
+with cte as(SELECT user_id, count(1)as tweet_count  FROM 
+tweets
+where EXTRACT(year from tweet_date)=2022
+group by user_id)
+select tweet_count as tweet_bucket ,count(user_id) as users_num
+from cte group by tweet_count
